4e9375a4a4a1f588715e8718c23d119072947f1b
em0nti <39101979+em0nti@users.noreply.github.com>
2024-09-25 14:55:35 +0300

    moved values of audio output options to constants

--- helpers/constants.js
+++ helpers/constants.js
@@ -1,3 +1,10 @@
 export const OPENAI_API_BASE_URL = 'https://api.openai.com/v1/';
 export const OPENAI_API_TRANSCRIPT_URL = 'audio/transcriptions';
 export const OPENAI_API_TRANSLATE_URL = 'audio/translations';
+export const OPENAI_API_WHISPER_INPUT_FILE_SIZE_RESTRICTION = 25;
+export const AUDIO_OUTPUT_OPTIONS = {
+	RATE: 44100,
+	CHANNELS: 1,
+	FORMAT: 'mp3',
+	BITRATE: '96k',
+};
