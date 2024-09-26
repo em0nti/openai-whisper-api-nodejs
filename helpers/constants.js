3886c736d430763bb90e48b819c854f430bacf59
em0nti <39101979+em0nti@users.noreply.github.com>
2024-09-26 11:26:45 +0300

    change audio output options

--- helpers/constants.js
+++ helpers/constants.js
@@ -2,9 +2,10 @@ export const OPENAI_API_BASE_URL = 'https://api.openai.com/v1/';
 export const OPENAI_API_TRANSCRIPT_URL = 'audio/transcriptions';
 export const OPENAI_API_TRANSLATE_URL = 'audio/translations';
 export const OPENAI_API_WHISPER_INPUT_FILE_SIZE_RESTRICTION = 25;
+
 export const AUDIO_OUTPUT_OPTIONS = {
-	RATE: 16000,
-	CHANNELS: 1,
-	FORMAT: 'wav',
-	BITRATE: '64k',
+	RATE: 48000,
+	CHANNELS: 2,
+	FORMAT: 'mp3',
+	BITRATE: '128k',
 };
