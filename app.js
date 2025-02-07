6e9091a6b04d2903194e2c1bcc3449e29485879c
em0nti <39101979+em0nti@users.noreply.github.com>
2025-02-07 09:40:29 +0200

    Clear transcription prompt in whisperTranscribe function

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ const writeTextToFile = (text, destinationPath) => {
 };
 
 async function whisperTranscribe(audioFilePath, outputPath, lang, format) {
-	const prompt = 'Товариство Червоного Хреста України,ТЧХУ,МРО, міжрегіональний, офіс.-';
+	const prompt = '';
 	try {
 		console.log('Start transcribing...');
 		const transcription = await openai.audio.transcriptions.create({
